fc1bd068bbbb39317383c4b444758499383a9e39
Grant Richard Steiner <grantsteiner64@gmail.com>
2018-01-14 13:20:21 -0600

    prelim flask code

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -2,10 +2,14 @@
 
 """
 
-import flask
-import requests
 import json
 import hashlib
+
+from textwrap import dedent
+from time import time
+from uuid import uuid4
+
+from flask import Flask, jsonify
 
 
 class Blockchain(object):
@@ -55,6 +59,36 @@ class Blockchain(object):
 
         return self.last_block["index"] + 1
 
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm
+            - Find a number p' such that hash(pp') contains 4 leading zeroes, where p is the previous p'
+            - p is the previous proof, and p' is the new proof
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof=last_proof, proof=proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+
+        :param last_proof: <int> previous proof
+        :param proof: <int> current proof
+        :return: <bool> True if correct, False if not
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
     @staticmethod
     def hash(block):
         """
@@ -67,7 +101,7 @@ class Blockchain(object):
         # We must make sure that the Dictionary is Ordered, or we will have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
-    
+
     @property
     def last_block(self):
         """ Returns the last block of the chain
@@ -75,3 +109,37 @@ class Blockchain(object):
         :return:
         """
         pass
+
+
+# instantiate our Node
+app = Flask(__name__)
+
+# generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# instantiate the Blockchain
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=["GET"])
+def mine():
+    return "We'll mine a new Block"
+
+
+@app.route("/transactions/new", methods=["POST"])
+def new_transaction():
+    return "We'll add a new transaction"
+
+
+@app.route("/chain", methods=["GET"])
+def full_chain():
+    response = {
+        "chain": blockchain.chain,
+        "length": len(blockchain.chain),
+    }
+
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",  port=5000)
